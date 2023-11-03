afd1cd5e8be95c0beb640bf7bfd1fecfd7bfeafc
viljamilaakso <127877285+viljamilaakso@users.noreply.github.com>
2023-11-03 15:21:44 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import CocktailInfo from "./CocktailInfo";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CocktailInfo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
